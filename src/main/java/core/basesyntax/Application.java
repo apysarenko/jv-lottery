c7a4e11986f184b14a45c0b0cde137099af6c3be
Anatolii <pisarenkotolya@gmail.com>
2025-11-13 23:14:15 +0200

    Виправив деякі помилки

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -2,8 +2,10 @@ package core.basesyntax;
 
 public class Application {
     public static void main(String[] args) {
+
+        Lottery lottery = new Lottery();
+
         for (int i = 0; i < 3; i++) {
-            Lottery lottery = new Lottery();
             Ball ball = lottery.getRandomBall();
             System.out.println(ball);
         }
